f7b130f5da389b928e5700062d819255c1f6c34a
Arthur Vaïsse-Lesteven <arthurvaisse@yahoo.fr>
2019-11-04 08:29:41 +0100

    Enhanced UI sidebar

--- src/main/java/com/avale/controllers/FileTabController.java
+++ src/main/java/com/avale/controllers/FileTabController.java
@@ -1,22 +1,42 @@
 package com.avale.controllers;
 
 import com.avale.model.Configuration;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 
 public class FileTabController extends Controller {
 	@FXML
-	TextArea textArea;
+	TextArea          configurationText;
+	@FXML
+	ChoiceBox<String> algorithm;
+	@FXML
+	TextField         encryptIteration;
+	@FXML
+	TextField         masterPassword;
+
 	private Configuration configuration;
 
 	@Override
 	void initialize() {
-		validateWiringOf(textArea, TextArea.class, "file content text area");
+		validateWiringOf(configurationText, TextArea.class, "file content text area");
 	}
 
 	public void setConfiguration(Configuration configuration) {
 		this.configuration = configuration;
-		this.textArea.setText(configuration.text());
+		this.configurationText.setText(configuration.text());
+	}
+
+	@FXML
+	private void encryptSelection(final ActionEvent actionEvent) {
+
+	}
+
+	@FXML
+	private void decryptSelection(final ActionEvent actionEvent) {
+
 	}
 }
